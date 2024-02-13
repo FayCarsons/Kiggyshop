c716d6e2ac9575c98457e1b7acc927a4fbceb25f
Fay Carsons <faycarsons23@gmail.com>
2024-02-12 19:30:03 -0500

    rename elm tests, add kind and price tests

--- frontend/tests/App.elm
+++ frontend/tests/App.elm
@@ -1,4 +1,4 @@
-module Example exposing (..)
+module App exposing (..)
 
 import Cart exposing (cartDecoder)
 import Expect exposing (Expectation)
@@ -6,6 +6,10 @@ import Fuzz exposing (Fuzzer, int, list, string)
 import Json.Decode exposing (decodeString)
 import Stock exposing (stockDecoder)
 import Test exposing (..)
+import Stock exposing (kindDecoder)
+import Html.Attributes exposing (kind)
+import Stock exposing (ProductKind)
+import Stock exposing (kindToPrice)
 
 
 suite : Test
@@ -21,4 +25,12 @@ suite =
                 |> Expect.ok
         , fuzz string "Fuzz stock parser" <|
             \s -> decodeString stockDecoder s |> Expect.err
+        , test "Valid kind parser" <| 
+            \_ -> "\"BigPrint\"" |> decodeString kindDecoder |> Expect.ok 
+        , fuzz string "Fuzz kind parser" <| 
+            decodeString kindDecoder >> Expect.err
+        , test "Kind to price" <| 
+            \_ -> Stock.BigPrint |> kindToPrice |> Expect.equal 20
+        , test "String of price" <| 
+            \_ -> Stock.stringOfPrice 23 |> Expect.equal "$23"
         ]
